6c38d581abc9e7761c16bf8c529d26c8cf9cbcd5
root <alicia0729@gmail.com>
2014-07-24 19:52:53 +0900

    original disksim-4.0

---
+++ diskmodel/layout_g1_load.d
@@ -0,0 +1,35 @@
+layout_g1_load.o: layout_g1_load.c /usr/include/stdlib.h \
+ /usr/include/features.h /usr/include/i386-linux-gnu/bits/predefs.h \
+ /usr/include/i386-linux-gnu/sys/cdefs.h \
+ /usr/include/i386-linux-gnu/bits/wordsize.h \
+ /usr/include/i386-linux-gnu/gnu/stubs.h \
+ /usr/include/i386-linux-gnu/gnu/stubs-32.h \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include/stddef.h \
+ /usr/include/i386-linux-gnu/bits/waitflags.h \
+ /usr/include/i386-linux-gnu/bits/waitstatus.h /usr/include/endian.h \
+ /usr/include/i386-linux-gnu/bits/endian.h \
+ /usr/include/i386-linux-gnu/bits/byteswap.h \
+ /usr/include/i386-linux-gnu/sys/types.h \
+ /usr/include/i386-linux-gnu/bits/types.h \
+ /usr/include/i386-linux-gnu/bits/typesizes.h /usr/include/time.h \
+ /usr/include/i386-linux-gnu/sys/select.h \
+ /usr/include/i386-linux-gnu/bits/select.h \
+ /usr/include/i386-linux-gnu/bits/sigset.h \
+ /usr/include/i386-linux-gnu/bits/time.h \
+ /usr/include/i386-linux-gnu/sys/sysmacros.h \
+ /usr/include/i386-linux-gnu/bits/pthreadtypes.h /usr/include/alloca.h \
+ /usr/include/string.h /usr/include/xlocale.h \
+ ../libparam/include/libparam/libparam.h /usr/include/stdio.h \
+ /usr/include/libio.h /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/lib/gcc/i686-linux-gnu/4.6/include/stdarg.h \
+ /usr/include/i386-linux-gnu/bits/stdio_lim.h \
+ /usr/include/i386-linux-gnu/bits/sys_errlist.h \
+ ../libparam/include/libparam/bitvector.h dm.h dm_config.h dm_types.h \
+ /usr/include/inttypes.h /usr/lib/gcc/i686-linux-gnu/4.6/include/stdint.h \
+ /usr/include/stdint.h /usr/include/i386-linux-gnu/bits/wchar.h \
+ ../libddbg/include/libddbg/libddbg.h layout_g1.h modules/modules.h \
+ modules/dm_disk_param.h modules/dm_layout_g1_param.h \
+ modules/dm_layout_g1_zone_param.h modules/dm_layout_g2_param.h \
+ modules/dm_layout_g2_zone_param.h modules/dm_layout_g4_param.h \
+ modules/dm_mech_g1_param.h modules/dm_layout_g1_param.h \
+ modules/dm_layout_g1_zone_param.h
